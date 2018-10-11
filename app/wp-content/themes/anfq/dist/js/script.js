244d701b52e17e7e8b3c1d0e57a0911b741c2391
nahoc-petal <ccarpentier@petalmd.com>
2018-10-11 11:05:22 -0400

    mobile menu

--- app/wp-content/themes/anfq/dist/js/script.js
+++ app/wp-content/themes/anfq/dist/js/script.js
@@ -1,13 +1,22 @@
 /**
  * File faq.js.
  */
-jQuery(function($) {
-  $(document).ready(function() {
-    $('.content').hide();
+jQuery(function ($) {
+  $(document).ready(function () {
 
-    $('.more').click(function() {
-      $(this).parent().parent().next('.content').slideToggle();
-      $(this).toggleClass('active');
+    $(".navbar-burger").click(function () {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
     });
+
+    if($('body').hasClass('page-template-neurofibromatosis')) {
+      $('.content').hide();
+
+      $('.more').click(function () {
+        $(this).parent().parent().next('.content').slideToggle();
+        $(this).toggleClass('active');
+      });
+    }
+    
   });
 });
